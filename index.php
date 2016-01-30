78dc85f244b95e2733a6450f73b1d59a6286b1b4
Eric Kuha <erickuha@gmail.com>
2016-01-30 16:12:21 -0600

    Very basic interface

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Agriduino Web Interface Root Page
+ * V 0.1
+ * User: eric
+ * Date: 1/30/16
+ * Time: 3:38 PM
+ */
+
+use League\Csv\Reader;
+
+
+echo "<h1>Welcome to GnomeInc&reg</h1>";
+
+$reader = Reader::createFromPath('var/www/agri_data.csv');
+foreach ($reader as $index => $row) {
+    echo $row;
+}
